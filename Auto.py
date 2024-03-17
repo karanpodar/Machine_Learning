39f0a46bbc00f1cab6df3d751985de91c671aa56
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-03-18 00:08:14 +0530

    Deletes

--- Auto.py
+++
@@ -1,6 +0,0 @@
-from autocorrect import Speller
-
-sp = Speller()
-sp("I'm not sleapy and tehre is no place I'm giong to.")
-
-print(sp)
--- req.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-req = requests.get('http://localhost:8080/candidates?text=I am the begt spell cherken')
-
-print(req.text)
-print('EOF')
