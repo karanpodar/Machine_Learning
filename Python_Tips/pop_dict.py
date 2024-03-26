17bcaf966d2ab492643370ecba106b3077452a9c
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-03-27 00:33:16 +0530

    pop

---
+++ Python_Tips/pop_dict.py
@@ -0,0 +1,9 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+car.pop("model")
+
+print(car)
---
+++ Python_Tips/pop_list.py
@@ -0,0 +1,8 @@
+my_list = [1, 2, 3, 4] 
+print("Popped element:", my_list.pop()) 
+print("List after pop():", my_list)
+
+my_list1 = [1, 2, 3, 4, 5, 6] 
+# Pops and removes the 3th index 
+# element from the list 
+print(my_list1.pop(3), my_list1)
