25afb8014529d9b266b2cbf608f9a03d4f300da6
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-03-31 00:13:03 +0530

    Random Forest

---
+++ Machine Learning/Classification Algo/Class_ML_RandFor_Single.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+churn_df = pd.read_csv(r'C:\Users\KARAN\Desktop\Python\VsCode_py\python_learn\Machine Learning\Classification\customer_churn.csv')
+
+churn_df = churn_df.drop(['RowNumber', 'CustomerId', 'Surname'], axis=1)
+
+numerical = churn_df.drop(['Geography', 'Gender'], axis=1)
+categorical = churn_df.filter(['Geography', 'Gender'])
+
+cat_numerical = pd.get_dummies(categorical[['Gender', 'Geography']], drop_first=False).astype(int)
+
+# splitting X and y
+X = pd.concat([numerical, cat_numerical], axis=1).drop(['Exited'], axis=1)
+
+y = numerical.filter(['Exited'])
+
+# Divide Data into Training and Test Sets
+X_train, X_test, y_train, y_test = train_test_split(X, y,  test_size=0.30, random_state=0)
+
+# Data Scaling/Normalization
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform (X_test)
+
+
+############## Random Forest #################
+
+rf_clf = RandomForestClassifier(random_state=42, n_estimators=500)
+
+classifier = rf_clf.fit(X_train, y_train)
+
+single_record = sc.transform (X.values[100].reshape(1, -1))
+predicted_churn = classifier.predict(single_record)
+
+print(X.iloc[100])
+print(y.iloc[100])
+print(predicted_churn)
