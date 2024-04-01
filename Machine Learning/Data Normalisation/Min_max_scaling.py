3da581dbdea322b231763e8f45e1a8e4b560b424
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-04-02 02:26:37 +0530

    Min Max scale

---
+++ Machine Learning/Data Normalisation/Min_max_scaling.py
@@ -0,0 +1,25 @@
+"""
+Data Normalisation is done for datasets which have huge differences in the values, like few values are in 100s and few in 10,000s
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+import numpy as np
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv(r'Machine Learning\Outlier_handling\winequality.csv')
+print(df.head())
+print(df.describe())
+
+sns.histplot(df['alcohol'])
+plt.show()
+
+# Min - Max scaling = (value - min) / (max - min)
+
+df_temp = df.copy()
+df_temp['alcohol'] = (df_temp['alcohol'] - df_temp['alcohol'].min()) / (df_temp['alcohol'].max() - df_temp['alcohol'].min())
+
+sns.histplot(df_temp['alcohol'])
+plt.show()
