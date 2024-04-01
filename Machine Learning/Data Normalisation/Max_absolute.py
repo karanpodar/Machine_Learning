36b2e3db9b4e583b2e56fced0e8bf9ec093ca7e7
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-04-02 02:26:28 +0530

    Max abs

---
+++ Machine Learning/Data Normalisation/Max_absolute.py
@@ -0,0 +1,25 @@
+"""
+Data Normalisation is done for datasets which have huge differences in the values, like few values are in 100s and few in 10,000s
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+import numpy as np
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv(r'Machine Learning\Outlier_handling\winequality.csv')
+print(df.head())
+print(df.describe())
+
+sns.histplot(df['free sulfur dioxide'])
+plt.show()
+
+# Max absolute scaling = value / max_value
+
+df_temp = df.copy()
+df_temp['free sulfur dioxide'] = df_temp['free sulfur dioxide'] / df_temp['free sulfur dioxide'].abs().max()
+
+sns.histplot(df_temp['free sulfur dioxide'])
+plt.show()
