d42a99dfc6ce393b9ae381e0064edf76188e13c8
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-04-21 12:37:55 +0530

    Null values

---
+++ Machine Learning/Handling Null Values/Fill_Mean.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r'Machine Learning\Encoding\Loan Prediction Dataset.csv')
+print(df.head())
+
+print(df['LoanAmount'].mean())
+
+# check null values
+print(df.isnull().sum())
+
+new_df = df.copy()
+
+# fill missing value for numerical
+new_df['LoanAmount'] = df['LoanAmount'].fillna(df['LoanAmount'].mean())
+print(new_df.isnull().sum())
