d274bcebcad34dffeb7a9d51a9afc264882214dc
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-05-19 01:15:11 +0530

    Python

---
+++ Python_Tips/DSA/Binary_search.py
@@ -0,0 +1,75 @@
+def binary_search(numbers_list, number_to_find):
+    left_index = 0
+    right_index = len(numbers_list) - 1
+    mid_index = 0
+
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
+
+        if mid_number == number_to_find:
+            return mid_index
+
+        if mid_number < number_to_find:
+            left_index = mid_index + 1
+        else:
+            right_index = mid_index - 1
+
+    return None
+
+def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
+    if right_index < left_index:
+        return None
+
+    mid_index = (left_index + right_index) // 2
+    if mid_index >= len(numbers_list) or mid_index < 0:
+        return None
+
+    mid_number = numbers_list[mid_index]
+
+    if mid_number == number_to_find:
+        return mid_index
+
+    if mid_number < number_to_find:
+        left_index = mid_index + 1
+    else:
+        right_index = mid_index - 1
+
+    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
+
+
+def find_all_occurances(numbers, number_to_find):
+  
+    index = binary_search(numbers, number_to_find)
+    indices = [index]
+    # find indices on left hand side
+    i = index-1
+    while i >=0:
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i - 1
+
+    # find indices on right hand side
+    i = index + 1
+    while i<len(numbers):
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i + 1
+
+    return sorted(indices)
+
+if __name__ == '__main__':
+    numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
+    number_to_find = 21
+
+    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
+    print(f"Number found at index {index} using binary search")
+
+    numbers = [1,4,6,9,11,15,15,15,17,21,34,34,56]
+    number_to_find = 15
+    indices = find_all_occurances(numbers, number_to_find)
+    print(f"Indices of occurances of {number_to_find} are {indices}")
