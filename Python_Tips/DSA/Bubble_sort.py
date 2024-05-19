7c7230e11b096b7ded515654452c864832165d28
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-05-20 00:09:31 +0530

    Python

---
+++ Python_Tips/DSA/Bubble_sort.py
@@ -0,0 +1,56 @@
+'''
+Bubble sort - take first 2 elements and swap them if not sorted, continue this till all elements are sorted
+In first iteration the largest number comes to the last position
+you can use this to sort strings too
+'''
+
+def bubble_sort(elements):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+def bubble_sort_key(elements, key):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            a = elements[j][key]
+            b = elements[j+1][key]
+            if a > b:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+
+if __name__ == '__main__':
+    elements = [5,9,2,1,67,34,88,34]
+    elements = [1,2,3,4,2]
+    elements = ["mona", "dhaval", "aamir", "tina", "chang"]
+
+    bubble_sort(elements)
+    print(elements)
+
+    elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]
+
+    bubble_sort_key(elements, key='transaction_amount')
+    print(elements)
