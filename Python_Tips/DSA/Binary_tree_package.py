78e41b0883d91ea36988a70d065a3e2295b61a79
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-05-20 00:09:24 +0530

    Python

---
+++ Python_Tips/DSA/Binary_tree_package.py
@@ -0,0 +1,14 @@
+from binarytree import build 
+
+# List of nodes 
+nodes =[3, 6, 8, 2, 11, None, 13] 
+  
+# Building the binary tree 
+binary_tree = build(nodes) 
+print('Binary tree from list :\n', 
+      binary_tree) 
+  
+# Getting list of nodes from 
+# binarytree 
+print('\nList from binary tree :',  
+      binary_tree.values) 
