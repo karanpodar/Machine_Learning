f779b1cdf697204f626e02b903d83678b682c91d
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-05-25 00:12:12 +0530

    Bag of words

--- Bag_of_Words/Text_split.py
+++ Bag_of_Words/Text_split.py
@@ -5,6 +5,9 @@ loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
 data = loader.load()
 print(data)
 print('*******************************************')
+
 text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(data)
 print(documents)
+
+#There are five levels of text splitting, each with different strategies and considerations. The levels are character splitting, recursive character text splitting, document-specific text splitting, semantic splitting, and agentic splitting.
