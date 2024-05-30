933061f023bbb6bfac1dcfcdabb6bb5eb9c7f176
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-05-30 13:12:42 +0530

    Langchain

---
+++ LangchainxHuggingFace/HF_Pipeline.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+model_id="gpt2"
+model=AutoModelForCausalLM.from_pretrained(model_id)
+tokenizer=AutoTokenizer.from_pretrained(model_id)
+
+pipe=pipeline("text-generation",model=model,tokenizer=tokenizer,max_new_tokens=100)
+hf=HuggingFacePipeline(pipeline=pipe)
+
+print(hf)
+
+print(hf.invoke("What is machine learning"))
