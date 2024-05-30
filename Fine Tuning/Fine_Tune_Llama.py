9c3cacf6b1e4d6fd6c8f13ac42b152682f36ea3c
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-05-31 00:02:22 +0530

    Fine Tune

---
+++ Fine Tuning/Fine_Tune_Llama.py
@@ -0,0 +1,14 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
