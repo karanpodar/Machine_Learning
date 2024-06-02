a4b233dc44661d59e82d3bace956a2230c5173b6
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-06-02 21:06:24 +0530

    Ensemble

---
+++ Machine Learning/Ensemble(Bagging & Boosting)/ADAboost.py
@@ -0,0 +1,41 @@
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Load the Iris dataset
+
+cancer = load_breast_cancer()
+X = cancer.data
+y = cancer.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Initialize the base classifier (in this case, a decision tree)
+base_classifier = DecisionTreeClassifier(random_state=2)
+
+dt_clf = base_classifier.fit(X_train, y_train)
+y_pred = dt_clf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+
+
+
+# Initialize the AdaBoost Classifier
+
+# You can specify the number of base estimators (n_estimators) and other parameters
+adaboost_clf = AdaBoostClassifier(dt_clf,random_state=2)
+
+# Train the AdaBoost
+adaboost_clf.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = adaboost_clf.predict(X_test)
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
