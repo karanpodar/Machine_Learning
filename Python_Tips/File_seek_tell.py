753add0cc86e26f231d184417926b14f65072243
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-06-10 01:14:44 +0530

    Python

---
+++ Python_Tips/File_seek_tell.py
@@ -0,0 +1,11 @@
+with open(r'Python_Tips\Python_linter.txt', 'r') as f:
+    
+    # Start reading the data from 5th position of the file -    
+    f.seek(5)
+
+    # read next 5 bytes
+    data = f.read(5)
+    print(data)
+
+    # tell function tells the current position of the cursor since we seeked 5 bytes and read 5 bytes tell will give an output of 10
+    print(f.tell())
