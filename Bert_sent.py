22226b4c0abb68e71219bd71b613c49a6b5a6fb3
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-06-15 20:12:11 +0530

    Python

--- Bert_sent.py
+++
@@ -1,5 +0,0 @@
-from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer('stsb-roberta-large')
-
-sentences = []
-embeddings = model.encode(sentences, convert_to_tensor=True)
