acb22aef34e74b4d0f5ad3d07fa8bd2e4c0d4117
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-06-17 19:30:08 +0530

    Python

---
+++ Python_Tips/OOPS/Hierarchial_inheritance.py
@@ -0,0 +1,12 @@
+"""
+Hierarchical inheritance when more than 1 subclass is derived from parent class
+"""
+
+class Base:
+    pass
+
+class Derived1(Base):
+    pass
+
+class Derived2(Base):
+    pass
