cb082508b385aa54bba96ecd27b103b8b6164e49
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-07-02 00:56:23 +0530

    Transformers

---
+++ Transformer Pipeline/QnA.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+question_answerer = pipeline("question-answering")
+question_answerer(
+    question="Where do I work?",
+    context="My name is Sylvain and I work at Hugging Face in Brooklyn",
+)
