db10cd07d6973944439728883790f77627d8d643
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-07-05 18:41:08 +0530

    python

--- Tensorflo.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-nn_mod = tf.keras.Sequential([
-    tf.keras.layers.Dense(32, activation='relu', input_shape=(10,)),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
