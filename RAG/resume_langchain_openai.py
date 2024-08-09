9eed0d837d685257464f6f2c3deda9e8addd25dc
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-08-09 19:05:54 +0530

    RAG

---
+++ RAG/resume_langchain_openai.py
@@ -0,0 +1,29 @@
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+with open(r'RAG\resume.txt', 'r', encoding="utf8") as f1:
+    resume_text = f1.read()
+
+vectorstore = FAISS.from_texts(
+    resume_text, embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+model = ChatOpenAI()
+
+retrieval_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+retrieval_chain.invoke("where did karan work?")
