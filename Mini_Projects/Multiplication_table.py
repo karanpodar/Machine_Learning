944bf0526ccfd8cedee28d712d57c9b147b3f97f
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-08-21 08:55:09 +0530

    Python

--- Mini_Projects/Multiplication_table.py
+++
@@ -1,9 +0,0 @@
-# Multiplication Table
-
-number = int(input("Please enter the Number\n"))
-
-print("The Multiplication table of number", number, "is:")
-for i in range(1,11):
-    print(number, "multiplied by", i, 'is equal to', (number * i))
-else:
-    print("Done")
