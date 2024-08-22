49add950666f3a17194f96b728c884ed75f2f182
karanpodar <156322358+karanpodar@users.noreply.github.com>
2024-08-22 17:52:02 +0530

    Python

--- Mini_Projects/reverse_multiplication_table.py
+++
@@ -1,9 +0,0 @@
-# Reverse Multiplication Table
-
-number = int(input("Please enter the Number\n"))
-
-print("The Multiplication table of number", number, "is:")
-for i in range(10,0,-1):
-    print(number, "multiplied by", i, 'is equal to', (number * i))
-else:
-    print("Done")
