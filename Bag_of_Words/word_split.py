6cdeb54f0a9fbd5d8bfe360649e54886ca381cf3
karanpodar <karanpodar@gmail.com>
2024-09-26 23:13:21 +0530

    NLP

---
+++ Bag_of_Words/word_split.py
@@ -0,0 +1,9 @@
+import wordninja
+'''
+https://github.com/keredson/wordninja
+'''
+
+
+text = "This isasentence. I am trying to splitthesewords."
+split_text = wordninja.split(text)
+print(split_text)
