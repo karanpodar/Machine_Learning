78092c853571fce70755037cfacd31e204ec4585
karanpodar <karanpodar@gmail.com>
2024-09-28 22:38:32 +0530

    Algo Expert

---
+++ Algo Expert/ceasar_encoder.py
@@ -0,0 +1,9 @@
+def caesarCipherEncryptor(string, key):
+    # Write your code here.
+    out = []
+    newkey = key % 26
+    for i in range(len(string)):
+        newVal = ord(string[i]) + newkey
+        out.append(chr(newVal) if newVal <= 122 else chr((newVal % 122) + 96))
+
+    return ''.join(out)
