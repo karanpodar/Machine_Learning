7755136aa2a07fa02f295e1a2809657849cb2877
Karan Poddar <156322358+karanpodar@users.noreply.github.com>
2024-10-26 16:20:00 +0530

    Update BankNotes.py

--- FastAPI/BankNotes.py
+++ FastAPI/BankNotes.py
@@ -5,6 +5,7 @@ Created on Tue Nov 17 21:51:19 2020
 @author: win10
 """
 from pydantic import BaseModel
+
 # 2. Class which describes Bank Notes measurements
 class BankNote(BaseModel):
     variance: float 
