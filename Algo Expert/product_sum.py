e49dd0bf797b06504552bd4df1e1d04ef07e934b
Karan Poddar <156322358+karanpodar@users.noreply.github.com>
2024-11-21 22:51:26 +0530

    Update product_sum.py

--- Algo Expert/product_sum.py
+++ Algo Expert/product_sum.py
@@ -1,6 +1,7 @@
 # Tip: You can use the type(element) function to check whether an item
 # is a list or an integer.
 def productSum(array, depth = 1):
+
     # Write your code here.
     sum = 0
     for i in array:
